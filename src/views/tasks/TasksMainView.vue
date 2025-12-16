294382827bc7f141cd3a2c5d5c97a37bf3871173
abbos-21 <abbos.nazariy0621@gmail.com>
2025-12-16 15:04:52 +0500

    commit 3

--- src/views/tasks/TasksMainView.vue
+++ src/views/tasks/TasksMainView.vue
@@ -8,12 +8,12 @@ import { CoinImage, TaskImage } from '@/assets/images'
     <div
       class="bg-[rgba(179,223,220,0.75)] rounded-xl flex justify-between items-center p-2 text-neutral-600"
     >
-      <div class="flex gap-2 items-center max-w-3/5">
+      <div class="flex gap-2 items-center">
         <img :src="TaskImage" class="w-12" alt="task-image" />
         <h1 class="font-bold">Join the BrunoPlay channel</h1>
       </div>
 
-      <div class="flex gap-4 items-center px-2">
+      <div class="flex gap-4 items-center px-2 shrink-0">
         <div class="flex gap-1 items-center rounded-full bg-[#FFAC33] px-2 py-px text-black">
           <img :src="CoinImage" class="w-6" alt="coin-image" />
           <span class="font-bold text-sm">50</span>
