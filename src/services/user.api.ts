8d1b3d609fd73d57dc0a75f58c91d43392fb56d9
abbos-21 <abbos.nazariy0621@gmail.com>
2025-12-17 19:49:11 +0500

    commit 15

--- src/services/user.api.ts
+++ src/services/user.api.ts
@@ -7,6 +7,6 @@ export interface UserResponse {
 
 export const UserAPI = {
   async getCurrentUser(): Promise<ApiResponse<UserResponse>> {
-    return http.post('/user/me')
+    return http.get('/user/me')
   },
 }
